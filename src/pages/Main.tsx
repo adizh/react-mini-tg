213f2e9d3e444a6092482cd98d98ba8797ff9f43
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-05-27 17:36:57 +0600

    main page added

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import LogoImg from '../assets/images/mdc-logo.png'
 function Main() {
-  return <div><p>
-    Main page
-  </p>
+    
+  return <div>
 
-<Link to='/react-mini-tg/about'>About link</Link>
+<div className="header">
+    <img className='logo-img' src={LogoImg} alt="" />
+    <h1 className='header-h1'>MyDataCoin</h1>
+    <p className='basic-text'>
+    This is the beginning of a reliable system
+    </p>
+</div>
+
+<div className='footer'>
+<button className="blue-btn">Create account</button>
+<button className="white-btn">Login</button>
+</div>
+
 
   </div>;
 }
