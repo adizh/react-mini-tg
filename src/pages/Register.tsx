0d3fd66290b90492f6ef62672c11619bc16557dd
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2024-05-30 19:57:42 +0600

    authorization init

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,24 @@
 
 import { useEffect } from 'react';
 import {useNavigate} from 'react-router-dom'
-function Login() {
+import { retrieveLaunchParams } from '@tma.js/sdk';
 
+function Register() {
+  const { initDataRaw, initData } = retrieveLaunchParams();
   const navigate = useNavigate();
   const tg=window.Telegram.WebApp;
+
+  const makeRequest = () => {
+    fetch('https://4a5e-31-186-54-131.ngrok-free.app/api/v1/User/authorize', {
+      method: 'POST',
+      headers: {
+        Authorization: `tma ${initDataRaw}`
+      },
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+  };
+
 
 tg.expand();
 const btn= tg.BackButton;
@@ -23,7 +37,6 @@ return ()=>{
     return <div className='login-section'>
       
       <h1>Create account</h1>
-  
       <p className='grey-text'>
       We have created a unique MyDataCin address 
 for you, which is similar to your telegram nickname
@@ -33,10 +46,10 @@ for you, which is similar to your telegram nickname
       <input type="text" className='basic-input'placeholder="Nickname" />
   
       <div className='bottom-part'>
-        <button className="blue-btn">Create</button>
+        <button onClick={makeRequest} className="blue-btn">Create</button>
       </div>
     </div>;
   }
   
-  export default Login;
+  export default Register;
   
