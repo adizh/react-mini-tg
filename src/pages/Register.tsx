8029718ea3247ab144cc70961ed570d8595060c1
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-05-31 11:50:53 +0600

    updates added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -20,6 +20,8 @@ function Register() {
   };
 
 
+  console.log('initDataRaw',initDataRaw)
+
 tg.expand();
 const btn= tg.BackButton;
 
@@ -35,16 +37,12 @@ return ()=>{
 }
 })
     return <div className='login-section'>
-      
       <h1>Create account</h1>
       <p className='grey-text'>
       We have created a unique MyDataCin address 
 for you, which is similar to your telegram nickname
       </p>
-  
-  
       <input type="text" className='basic-input'placeholder="Nickname" />
-  
       <div className='bottom-part'>
         <button onClick={makeRequest} className="blue-btn">Create</button>
       </div>
