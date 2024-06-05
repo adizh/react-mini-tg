c799186bccb816edead5865a4a068020f4349717
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-06-05 15:00:08 +0600

    loading state added for mining

--- src/components/Mining.tsx
+++ src/components/Mining.tsx
@@ -7,6 +7,7 @@ function Mining() {
     return storedValue ? parseInt(storedValue, 10) : 0;
   };
 
+  const [isMiningLoading,setIsMiningLoading]=useState(true)
 
   const localSavedMining= localStorage.getItem('isMiningStarted')  && localStorage.getItem('isMiningStarted')  ==='true' ? true :false
   const localSavedClaim= localStorage.getItem('claim')  && localStorage.getItem('claim')  ==='true' ? true :false
@@ -29,73 +30,96 @@ function Mining() {
   const startMining = () => {
     setStartMining(true);
     const currentUnixTime = Math.floor(new Date().getTime() / 1000);
+    setIsMiningLoading(false)
     localStorage.setItem('isMiningStarted','true')
     const twelveHoursLaterUnixTime = currentUnixTime + 12 * 60 * 60;
     const twoMinutesLaterUnixTime = currentUnixTime + 2 * 60;
-    setTwelveHoursLater(twoMinutesLaterUnixTime);
+    setTwelveHoursLater(twelveHoursLaterUnixTime);
     setCurrentTime(currentUnixTime);
-
     localStorage.setItem("currentUnixTime", currentUnixTime.toString());
     localStorage.setItem(
       "unixTimestampTwelveHoursLater",
-      twoMinutesLaterUnixTime.toString(),
+      twelveHoursLaterUnixTime.toString(),
     );
   };
 
   useEffect(() => {
-    if (!isMiningStarted) return;
+    if (!isMiningStarted){
+        setIsMiningLoading(false)
+      return;
+    }
 
     const totalDuration = 12 * 60 * 60; 
     const totalDuration2 = 2 * 60
     const intervalId = setInterval(() => {
       const currentUnixTime = Math.floor(new Date().getTime() / 1000);
       const newTimeLeft = twelveHoursLater - currentUnixTime;
-
       if (newTimeLeft <= 0) {
         clearInterval(intervalId);
         setTimeLeft('00:00:00');
         setPercentage(100);
         setStartMining(false)
         setClaim(true)
+        setIsMiningLoading(true)
         localStorage.setItem('isMiningStarted','false')
         localStorage.setItem('claim','true')
         localStorage.removeItem('currentUnixTime')
         localStorage.removeItem('twoMinutesLaterUnixTime')
         localStorage.removeItem('isMiningStarted')
-      
       } else {
+
+      
         const hours = Math.floor(newTimeLeft / 3600);
         const minutes = Math.floor((newTimeLeft % 3600) / 60);
         const seconds = newTimeLeft % 60;
         const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
         setTimeLeft(formattedTime);
 
-        const elapsed = totalDuration2 - newTimeLeft;
-        const percentageComplete =+((elapsed / totalDuration2) * 100).toFixed(2)
+        const elapsed = totalDuration - newTimeLeft;
+        const percentageComplete =+((elapsed / totalDuration) * 100).toFixed(2)
         console.log('percentageComplete',percentageComplete)
         console.log('newTimeLeft',newTimeLeft)
         console.log('elapsed',elapsed);
         console.log('totalDuration',totalDuration)
         setPercentage(+percentageComplete);
+        setIsMiningLoading(false)
       }
+
     }, 1000);
 
-    return () => clearInterval(intervalId);
+    return () =>{
+        clearInterval(intervalId)
+
+    };
   }, [isMiningStarted, twelveHoursLater]);
 
   return (
-    <div>
-      {isMiningStarted ? (
-        <PercentageLine percentage={percentage} />
-      ) : claim ?   <button className="blue-btn" onClick={startMining}>
-      Claim
-    </button>:  (
-        <button className="blue-btn" onClick={startMining}>
-          Start mining
-        </button>
-      )}
-      {isMiningStarted && <p className="grey-text">{timeLeft} left</p>}
-    </div>
+//     <div>
+//    {isMiningStarted ? (
+//      <PercentageLine percentage={percentage} />
+//     ) : claim ?   <button className="blue-btn" onClick={startMining}>
+//  Claim
+//    </button>:  (
+//       <button className="blue-btn" onClick={startMining}>
+//        Start mining
+//       </button>
+//      )}
+//      {isMiningStarted && <p className="grey-text">{timeLeft} left</p>}
+//    </div>
+ <>
+   {!isMiningLoading  ?  <div>
+    {isMiningStarted ? (
+      <PercentageLine percentage={percentage} />
+    ) : claim ?   <button className="blue-btn" onClick={startMining}>
+    Claim
+  </button>:  (
+      <button className="blue-btn" onClick={startMining}>
+        Start mining
+      </button>
+    )}
+    {isMiningStarted && <p className="grey-text">{timeLeft} left</p>}
+  </div> : <div className='grey-text'>Loading...</div>}
+ </>
   );
 }
 
