c840fed418ef90459dd0b102421b05714343623d
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-06-05 17:06:13 +0600

    terser build added

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,19 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   plugins: [react()],
   base: "/react-mini-tg/",
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+      },
+    },
+  },
 });
